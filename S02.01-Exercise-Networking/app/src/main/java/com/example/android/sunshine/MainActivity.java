bd1013e8e03c45e9389ed675673c256005d2a844
Keisuke Sekizawa <sekizawa@team-lab.com>
2017-09-19 12:13:14 +0900

    Delete the dummy weather data. You will be getting REAL data from the Internet in this lesson.

--- S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -34,30 +34,6 @@ public class MainActivity extends AppCompatActivity {
          */
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
-        // TODO (4) Delete the dummy weather data. You will be getting REAL data from the
-        // Internet in this lesson.
-        /*
-         * This String array contains dummy weather data. Later in the course, we're going to get
-         * real weather data. For now, we want to get something on the screen as quickly as
-         * possible, so we'll display this dummy data.
-         */
-        String[] dummyWeatherData = {
-                "Today, May 17 - Clear - 17°C / 15°C",
-                "Tomorrow - Cloudy - 19°C / 15°C",
-                "Thursday - Rainy- 30°C / 11°C",
-                "Friday - Thunderstorms - 21°C / 9°C",
-                "Saturday - Thunderstorms - 16°C / 7°C",
-                "Sunday - Rainy - 16°C / 8°C",
-                "Monday - Partly Cloudy - 15°C / 10°C",
-                "Tue, May 24 - Meatballs - 16°C / 18°C",
-                "Wed, May 25 - Cloudy - 19°C / 15°C",
-                "Thu, May 26 - Stormy - 30°C / 11°C",
-                "Fri, May 27 - Hurricane - 21°C / 9°C",
-                "Sat, May 28 - Meteors - 16°C / 7°C",
-                "Sun, May 29 - Apocalypse - 16°C / 8°C",
-                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-        };
-
         // TODO (9) Call loadWeatherData to perform the network request to get the weather
     }
 
